993fc2cc148f68215bf0fe107b959cd715ab7013
Ahmad Rifai <ahmmd.riffai@gmail.com>
2025-05-24 22:16:10 +0700

    fix: cors enable POST config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -55,7 +55,12 @@ func main() {
 	// middleware
 	app.Use(logger.New())
 	app.Use(recover.New())
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	// app.Use(cors.New(cors.Config{
 	// 	// AllowOriginsFunc: func(origin string) bool {
 	// 	// 	allowedOrigins := []string{
