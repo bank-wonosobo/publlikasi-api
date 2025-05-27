894169c55a66b81ee08497f3704985b8aaa7da3f
Ahmad Rifai <ahmmd.riffai@gmail.com>
2025-05-27 14:44:05 +0700

    feat: get all news

--- internal/delivery/http/dto/response/news_response.go
+++ internal/delivery/http/dto/response/news_response.go
@@ -3,15 +3,15 @@ package response
 import "time"
 
 type NewsResponse struct {
-	ID          string `gorm:"primaryKey"`
-	Title       string
-	Slug        string
-	Content     string
-	Author      string
-	ImageUrl    string
-	PublishedAt *time.Time
-	Status      string
-	ApprovedBy  *string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          string     `json:"id"`
+	Title       string     `json:"title"`
+	Slug        string     `json:"slug"`
+	Content     string     `json:"content"`
+	Author      string     `json:"author"`
+	ImageUrl    string     `json:"image_url"`
+	PublishedAt *time.Time `json:"published_at"`
+	Status      string     `json:"status"`
+	ApprovedBy  *string    `json:"approved_by"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
 }
